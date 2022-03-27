918b52479863fad1d60f3e276e6694a3e3fa9179
mimou925 <61367409+mimou925@users.noreply.github.com>
2022-03-27 14:48:26 +0200

    Add files via upload

---
+++ sketch_mar10a.ino
@@ -0,0 +1,54 @@
+#include "DFRobot_CCS811.h"
+
+#define ledGreen 5
+#define ledYellow 4
+#define ledRed 3
+
+DFRobot_CCS811 CCS811;
+
+int passage = 0;
+
+void setup() {
+    pinMode(ledGreen, OUTPUT);
+    pinMode(ledYellow, OUTPUT);
+    pinMode(ledRed, OUTPUT);
+
+    Serial.begin(115200);
+
+    while(CCS811.begin() != 0) {
+        Serial.println("Failed to init chip, please check the chip connection");
+    }
+}
+
+void loop() {
+    if(CCS811.getCO2PPM() >= 0 && CCS811.getCO2PPM() <= 499) {
+        digitalWrite(ledYellow, LOW);
+        digitalWrite(ledRed, LOW);
+        digitalWrite(ledGreen, HIGH);
+        passage = 0;
+    } else if(CCS811.getCO2PPM() >= 500 && CCS811.getCO2PPM() <= 999) {
+        digitalWrite(ledYellow, HIGH);
+        digitalWrite(ledRed, LOW);
+        digitalWrite(ledGreen, LOW);
+        if(passage == 0) {
+            tone(6, 600);
+            delay(500);
+            noTone(6);
+        }
+        passage = 1;
+    } else if(CCS811.getCO2PPM() >= 1000) {
+        digitalWrite(ledYellow, LOW);
+        digitalWrite(ledRed, HIGH);
+        digitalWrite(ledGreen, LOW);
+        if(passage == 0 || passage == 1) {
+            tone(6, 600);
+            delay(1000);
+            noTone(6);
+        }
+        passage = 2;
+    }
+    
+    CCS811.writeBaseLine(0x447B);
+    delay(1000);
+    
+}
